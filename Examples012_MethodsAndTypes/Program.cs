abadd0d15e06a54c1434b866d2707ccf3dbd4033
«dl0xa <dl0xa@mail.ru>
2023-02-05 15:19:16 +0300

    Lesson 3 done HW

--- Examples012_MethodsAndTypes/Program.cs
+++ Examples012_MethodsAndTypes/Program.cs
@@ -112,7 +112,7 @@ string text = "-Я думаю, - сказал князь, улыбаясь, - ч
 // System.Console.WriteLine(newText);
 
 
-// Упорядочивание массива
+// Упорядочивание массива в обратном порядке
 
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 void PrintArray(int[] array)
@@ -129,18 +129,17 @@ void SelectionSort(int[]array)
 {
     for (int i = 0; i < array.Length-1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[maxPosition]) maxPosition = j;
         }
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 
 }
 PrintArray(arr);
 SelectionSort(arr);
-
 PrintArray(arr);
